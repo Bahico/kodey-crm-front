0c544934b5529785b55a4baca45a7effa542484a
ikrom <ikromov0312@gmail.com>
2026-03-02 00:14:29 +0500

    added finance

--- src/app/pages/dashboard/pages/finances/finances.ts
+++ src/app/pages/dashboard/pages/finances/finances.ts
@@ -1,7 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {CustomScrollbar} from '@/shared/components/custom-scrollbar/custom-scrollbar';
+import {injectRegisterIcons, SvgIconComponent} from '@ngneat/svg-icon';
+import {rightSquareIcon} from '@/svg/right-square';
+import {filterIcon} from '@/svg/filter';
+import {NgClass} from '@angular/common';
+import {TuiDropdown} from '@taiga-ui/core';
+import {TuiActiveZone, TuiObscured} from '@taiga-ui/cdk';
+import {FormsModule} from '@angular/forms';
+import {TuiCheckbox} from '@taiga-ui/kit';
 
 @Component({
   templateUrl: 'finances.html',
-  selector: 'app-finances',
+  imports: [
+    CustomScrollbar,
+    SvgIconComponent,
+    NgClass,
+    TuiDropdown,
+    TuiObscured,
+    TuiActiveZone,
+    FormsModule,
+    TuiCheckbox
+  ],
+  selector: 'app-finances'
 })
-export default class FinancesComponent {}
+export default class FinancesComponent {
+  protected readonly activeYear = signal<number>(null);
+  protected readonly activeMonth = signal<number>(null);
+  protected readonly open = signal(false);
+
+  constructor() {
+    injectRegisterIcons([
+      rightSquareIcon,
+      filterIcon
+    ]);
+  }
+
+  protected onClick(): void {
+    this.open.set(!this.open());
+  }
+
+  protected onObscured(obscured: boolean): void {
+    if (obscured) {
+      this.open.set(false);
+    }
+  }
+
+  protected onActiveZone(active: boolean): void {
+    this.open.set(active && this.open());
+  }
+}
